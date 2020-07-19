720360fe47dcdabd81b677687a9ce12b9ded0bec
AWhiteFox <ignat.k.150@gmail.com>
2020-07-19 15:18:06 +0300

    IListExtensions.cs

---
+++ Caved In/Assets/Scripts/Extensions/IListExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+public static class IListExtensions
+{
+    public static T Pop<T>(this IList<T> list, int index)
+    {
+        T result = list[index];
+        list.RemoveAt(index);
+        return result;
+    }
+}
