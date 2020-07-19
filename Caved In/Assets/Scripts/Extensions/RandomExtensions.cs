eb6b10b25ff780977951648b6a6a107f319e8c1f
AWhiteFox <ignat.k.150@gmail.com>
2020-07-19 15:18:30 +0300

    RandomExtensions.cs

---
+++ Caved In/Assets/Scripts/Extensions/RandomExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class RandomExtensions
+{
+    public static T PickFrom<T>(this Random random, IList<T> list)
+    {
+        return list[random.Next(list.Count)];
+    }
+
+    public static T PopFrom<T>(this Random random, IList<T> list)
+    {
+        return list.Pop(random.Next(list.Count));
+    }
+}
