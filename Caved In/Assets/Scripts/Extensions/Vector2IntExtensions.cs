e70cb5fb30eb3cbc6a96830fb60df07148575bcc
AWhiteFox <ignat.k.150@gmail.com>
2020-07-26 22:30:13 +0300

    Get rid of old room system

--- Caved In/Assets/Scripts/Extensions/Vector2IntExtensions.cs
+++ Caved In/Assets/Scripts/Extensions/Vector2IntExtensions.cs
@@ -12,25 +12,4 @@ public static class Vector2IntExtensions
             vector + Vector2Int.left
         };
     }
-
-    public static Directions ToDirection(this Vector2Int vector)
-    {
-        if (vector == Vector2.up)
-        {
-            return Directions.North;
-        }
-        else if (vector == Vector2Int.right)
-        {
-            return Directions.East;
-        }
-        else if (vector == Vector2Int.down)
-        {
-            return Directions.South;
-        }
-        else if (vector == Vector2Int.left)
-        {
-            return Directions.West;
-        }
-        throw new ArgumentException();
-    }
 }
--- Caved In/Assets/Scripts/TileSelectors/WallTileSelector.cs
+++ Caved In/Assets/Scripts/TileSelectors/WallTileSelector.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Tilemaps;
 
 [CreateAssetMenu(fileName = nameof(WallTileSelector), menuName = "ScriptableObjects/TileSelectors/Wall")]
@@ -18,33 +17,34 @@ public class WallTileSelector : TileSelectorBase
     public TileBase wallCornerOuterSE;
     public TileBase wallCornerOuterSW;
 
-    public override TileBase Pick(int x, int y, Room room)
-    {
-        int mask = 0;
-        mask += room.GetTile(x, y + 1) == RoomTile.Wall ? 1 : 0;
-        mask += room.GetTile(x + 1, y) == RoomTile.Wall ? 2 : 0;
-        mask += room.GetTile(x, y - 1) == RoomTile.Wall ? 4 : 0;
-        mask += room.GetTile(x - 1, y) == RoomTile.Wall ? 8 : 0;
+    // TODO Make this work with new map
+    //public override TileBase Pick(int x, int y, Room room)
+    //{
+    //    int mask = 0;
+    //    mask += room.GetTile(x, y + 1) == RoomTile.Wall ? 1 : 0;
+    //    mask += room.GetTile(x + 1, y) == RoomTile.Wall ? 2 : 0;
+    //    mask += room.GetTile(x, y - 1) == RoomTile.Wall ? 4 : 0;
+    //    mask += room.GetTile(x - 1, y) == RoomTile.Wall ? 8 : 0;
 
-        switch (mask)
-        {
-            // Walls on edge
-            case 1: goto case 5;
-            case 2: goto case 10;
-            case 4: goto case 5;
-            case 8: goto case 10;
+    //    switch (mask)
+    //    {
+    //        // Walls on edge
+    //        case 1: goto case 5;
+    //        case 2: goto case 10;
+    //        case 4: goto case 5;
+    //        case 8: goto case 10;
 
-            // Walls
-            case 5: return room.GetTile(x - 1, y) == RoomTile.Void ? wallW : wallE;
-            case 10: return room.GetTile(x, y + 1) == RoomTile.Void ? wallN : wallS;
+    //        // Walls
+    //        case 5: return room.GetTile(x - 1, y) == RoomTile.Void ? wallW : wallE;
+    //        case 10: return room.GetTile(x, y + 1) == RoomTile.Void ? wallN : wallS;
 
-            // Corners
-            case 3: return room.GetTile(x, y - 1) == RoomTile.Void ? wallCornerOuterSW : wallCornerInnerNE;
-            case 6: return room.GetTile(x, y + 1) == RoomTile.Void ? wallCornerOuterNW : wallCornerInnerSE;
-            case 12: return room.GetTile(x, y + 1) == RoomTile.Void ? wallCornerOuterNE : wallCornerInnerSW;
-            case 9: return room.GetTile(x, y - 1) == RoomTile.Void ? wallCornerOuterSE : wallCornerInnerNW;
+    //        // Corners
+    //        case 3: return room.GetTile(x, y - 1) == RoomTile.Void ? wallCornerOuterSW : wallCornerInnerNE;
+    //        case 6: return room.GetTile(x, y + 1) == RoomTile.Void ? wallCornerOuterNW : wallCornerInnerSE;
+    //        case 12: return room.GetTile(x, y + 1) == RoomTile.Void ? wallCornerOuterNE : wallCornerInnerSW;
+    //        case 9: return room.GetTile(x, y - 1) == RoomTile.Void ? wallCornerOuterSE : wallCornerInnerNW;
 
-            default: Debug.LogWarning(mask); throw new ArgumentException("Impossible wall position", nameof(room));
-        }
-    }
+    //        default: Debug.LogWarning(mask); throw new ArgumentException("Impossible wall position", nameof(room));
+    //    }
+    //}
 }
