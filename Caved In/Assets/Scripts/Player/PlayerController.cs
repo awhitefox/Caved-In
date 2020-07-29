27867f98d07f2c01f1328a82addaa0ae63cbef57
AWhiteFox <ignat.k.150@gmail.com>
2020-07-29 15:49:15 +0300

    PLayer controller

---
+++ Caved In/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = movement.normalized * movementSpeed;
+    }
+}
